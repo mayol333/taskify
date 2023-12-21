c24acc90be2dc2f928b809ee61307d74831695b4
mayol333 <s.moniczewski@gmail.com>
2023-12-21 20:35:52 +0100

    logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import Logo from "./assets/logo-icon.svg?react"
 export const App = () =>  {
   return (
-    <div></div>
+    <div className="master-wrapper">
+      <header className="header">
+      <div className="logo"><Logo/></div>
+      <span className="taskify-logo">TASKIFY</span>
+      </header>
+      <div className="test"></div>
+    </div>
   )
 }
 
