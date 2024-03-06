8d4f5ed204c2ebf6627b10437b404d2c4d6fd27a
mayol333 <s.moniczewski@gmail.com>
2024-03-06 21:00:30 +0100

    chart

--- src/components/reports/Reports.tsx
+++ src/components/reports/Reports.tsx
@@ -1,9 +1,23 @@
+import * as echarts from "echarts";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { ChangeEventHandler, useEffect, useRef, useState } from "react";
 import { Task } from "../tasks/Tasks";
 import { MeetingType } from "../ui/Meeting/type";
+import { Select } from "../ui/Select/Select";
+import {
+    startOfWeek,
+    endOfWeek,
+    startOfMonth,
+    endOfMonth,
+    eachDayOfInterval,
+    format,
+} from "date-fns";
+interface ChartData {
+    dates: string[];
+    hours: number[];
+}
+type Period = "today" | "thisWeek" | "thisMonth";
 const getTimeStamp = (date: string) => new Date(date).getTime();
-
 const parseDayData = (date: string, tasks: Task[], meetings: MeetingType[]) => {
     const matchedTasks = tasks.filter((task) => task.date === date);
     const matchedMeetings = meetings.filter((meeting) => meeting.date === date);
@@ -13,7 +27,6 @@ const parseDayData = (date: string, tasks: Task[], meetings: MeetingType[]) => {
     const meetingTotalTime = matchedMeetings.reduce((total, start) => {
         return differenceInHours(start.startTime, start.endTime) + total;
     }, 0);
-    console.log(taskTotalTime + meetingTotalTime);
     return taskTotalTime + meetingTotalTime;
 };
 const convertTimeToNumber = (time: string) => {
@@ -32,7 +45,9 @@ const parseSpanData = (
     tasks: Task[],
     meetings: MeetingType[]
 ) => {
-    const [startDate, endDate] = span;
+    // const [startDate, endDate] = span;
+    const startDate = span[0];
+    const endDate = span[span.length - 1];
     const matchedTasks = tasks.filter(
         (task) =>
             getTimeStamp(task.date) >= getTimeStamp(startDate) &&
@@ -43,17 +58,75 @@ const parseSpanData = (
             getTimeStamp(meeting.date) >= getTimeStamp(startDate) &&
             getTimeStamp(meeting.date) <= getTimeStamp(endDate)
     );
-    const taskTotalTime = matchedTasks.reduce((total, start) => {
-        return differenceInHours(start.startTime, start.endTime) + total;
-    }, 0);
-    const meetingTotalTime = matchedMeetings.reduce((total, start) => {
-        return differenceInHours(start.startTime, start.endTime) + total;
-    }, 0);
-    console.log(taskTotalTime + meetingTotalTime);
-    return taskTotalTime + meetingTotalTime;
+    // const taskTotalTime = matchedTasks.reduce((total, start) => {
+    //     return differenceInHours(start.startTime, start.endTime) + total;
+    // }, 0);
+    // const meetingTotalTime = matchedMeetings.reduce((total, start) => {
+    //     return differenceInHours(start.startTime, start.endTime) + total;
+    // }, 0);
+    // return taskTotalTime + meetingTotalTime;
+    const parsedTasks = matchedTasks.map((task) => {
+        return {
+            date: task.date,
+            hours: differenceInHours(task.startTime, task.endTime),
+        };
+    });
+    const parsedMeetings = matchedMeetings.map((meeting) => {
+        return {
+            date: meeting.date,
+            hours: differenceInHours(meeting.startTime, meeting.endTime),
+        };
+    });
+    const result = [...parsedTasks, ...parsedMeetings].reduce(
+        (total: Array<string[] & number[]>, start) => {
+            if (total[0].includes(start.date)) {
+                const index = total[0].indexOf(start.date);
+                total[1][index] += start.hours;
+            } else {
+                total[0].push(start.date);
+                total[1].push(start.hours);
+            }
+            return total;
+        },
+        [[], []]
+    );
+    console.log({ parsedTasks });
+    console.log({ parsedMeetings });
+    console.log({ result });
+};
+const timePeriod = (period: Period) => {
+    switch (period) {
+        case "today":
+            return [format(new Date(), "yyyy-MM-dd")];
+        case "thisWeek": {
+            const now = new Date();
+            const start = startOfWeek(now, { weekStartsOn: 1 });
+            const end = endOfWeek(now, { weekStartsOn: 1 });
+            const datesOfTheWeek = eachDayOfInterval({ start, end }).map(
+                (date) => format(date, "yyyy-MM-dd")
+            );
+            return datesOfTheWeek;
+        }
+        case "thisMonth": {
+            const now = new Date();
+            const start = startOfMonth(now);
+            const end = endOfMonth(now);
+            const datesOfTheMonth = eachDayOfInterval({ start, end }).map(
+                (date) => format(date, "yyyy-MM-dd")
+            );
+            return datesOfTheMonth;
+        }
+    }
 };
 export const Reports = () => {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState<ChartData | null>(null);
+    const [select, setSelect] = useState<Period>("today");
+    const chartRef = useRef(null);
+    const handleSelect: ChangeEventHandler<HTMLSelectElement> = (event) => {
+        const { value } = event.target;
+        setSelect(value as Period);
+        timePeriod(value as Period);
+    };
     useEffect(() => {
         const getData = async () => {
             try {
@@ -62,14 +135,18 @@ export const Reports = () => {
                     axios.get("http://localhost:8000/meetings"),
                 ]);
                 const [tasks, meetings] = data;
-                // parseDayData("2024-01-20", tasks.data, meetings.data);
-                // differenceInHours("17:30", "19:00");
-                parseSpanData(
-                    ["2024-01-17", "2024-01-20"],
-                    tasks.data,
-                    meetings.data
-                );
-                return data;
+                const date = [
+                    "2024-01-17",
+                    "2024-01-18",
+                    "2024-01-19",
+                    "2024-01-20",
+                ];
+                // const count = parseDayData(date, tasks.data, meetings.data);
+                const count = parseSpanData(date, tasks.data, meetings.data);
+                setData({
+                    dates: date,
+                    hours: [count],
+                });
             } catch (error) {
                 console.log(error);
                 return [];
@@ -77,5 +154,61 @@ export const Reports = () => {
         };
         getData();
     }, []);
-    return <p>Reports</p>;
+    useEffect(() => {
+        const chart = echarts.init(chartRef.current);
+        const { dates, hours } = data || {};
+        if (select === "today") {
+            parseDayData();
+        } else {
+            parseSpanData();
+        }
+        const options = {
+            xAxis: {
+                type: "category",
+                data: dates,
+            },
+            yAxis: {
+                type: "value",
+            },
+            series: [
+                {
+                    data: hours,
+                    type: "bar",
+                },
+            ],
+        };
+        chart.setOption(options);
+        return () => {
+            chart.dispose();
+        };
+    }, [data, select]);
+    useEffect(() => {
+        const now = new Date();
+        const start = startOfWeek(now, { weekStartsOn: 1 });
+        const end = endOfWeek(now, { weekStartsOn: 1 });
+        const datesOfTheWeek = eachDayOfInterval({ start, end }).map((date) =>
+            format(date, "yyyy-MM-dd")
+        );
+        console.log(datesOfTheWeek);
+    });
+    return (
+        <div className="master-container">
+            <h2 className="charts-headline">Reports</h2>
+            <div className="select-container">
+                <Select
+                    placeholder="Time period"
+                    options={[
+                        { label: "Today", value: "today" },
+                        { label: "This week", value: "thisWeek" },
+                        { label: "This month", value: "thisMonth" },
+                    ]}
+                    handleSelect={handleSelect}
+                    value={select}
+                />
+            </div>
+            <div className="reports-container">
+                <div ref={chartRef} className="chart-container"></div>
+            </div>
+        </div>
+    );
 };
